2c047ee582cee65bb6454a9491a57dbc631fad1d
Ernst Fluttert <e.fluttert@gmail.com>
2016-04-24 22:40:50 +0200

    added connected components within graphs

---
+++ DirectGraphTests/ComponentsTests.cs
@@ -0,0 +1,32 @@
+﻿using Fluttert.DirectGraph;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluttert.DirectGraphTests
+{
+    [TestClass]
+    class ComponentsTests
+    {
+        protected static Graph graph;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            graph = new Graph(5);
+            graph.AddEdge(0, 1);
+            graph.AddEdge(2, 3);
+        }
+
+
+        [TestMethod]
+        public void GraphConnectedComponents() {
+            var g = graph.DeepCopy();
+
+            //int[] components = 
+        }
+    }
+}
