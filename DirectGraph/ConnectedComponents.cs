aae7243e55beb7a12c5dd6a1241bed3d09407e7d
Ernst Fluttert <e.fluttert@gmail.com>
2016-04-25 22:25:59 +0200

    documented ConnectedComponents

--- DirectGraph/ConnectedComponents.cs
+++ DirectGraph/ConnectedComponents.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Fluttert.DirectGraph
 {
@@ -10,26 +11,64 @@ namespace Fluttert.DirectGraph
     {
         private int amountOfComponents;
         private int[] componentIds;
+        private int vertices;
 
         /// <summary>
         /// Construct the connected components based on an undirected graph
         /// </summary>
-        /// <param name="graph"></param>
+        /// <param name="graph">Graph to be examined</param>
         public ConnectedComponents(UndirectedGraph graph)
         {
             componentIds = new int[graph.Vertices()];
             amountOfComponents = 0;
+            vertices = graph.Vertices();
         }
 
-        public int ComponentId(int vertex) => componentIds[vertex];
+        /// <summary>
+        /// Based a vertex give back the corresponding Component ID
+        /// </summary>
+        /// <param name="vertex">The vertexId in the graph</param>
+        /// <returns>int, component ID</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when vertex is out of range</exception>
+        public int ComponentId(int vertex)
+        {
+            if (vertex < 0 || vertex >= vertices)
+            {
+                throw new ArgumentOutOfRangeException($"vertex id {vertex} is out of range. Range is: 0 -  {vertices - 1 }");
+            }
+            return componentIds[vertex];
+        }
 
+        /// <summary>
+        /// Total amount of components for this graph
+        /// </summary>
+        /// <returns>int</returns>
         public int AmountOfComponents() => amountOfComponents;
 
+        /// <summary>
+        /// Dertermine if 2 vertices are in the same connected component
+        /// </summary>
+        /// <param name="vertex1">ID of vertex 1</param>
+        /// <param name="vertex2">ID of vertex 2</param>
+        /// <returns>bool: true on connected, false otherwise</returns>
         public bool Connected(int vertex1, int vertex2) => ComponentId(vertex1) == ComponentId(vertex2);
 
-        public List<int> GetVerticesForComponent(int componentId) {
+        /// <summary>
+        /// Given a component ID, return all vertices
+        /// </summary>
+        /// <param name="componentId">The component ID</param>
+        /// <returns>List of integers with the vertices belonging to this component</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when componentId does not exist</exception>
+        public List<int> GetVerticesForComponent(int componentId)
+        {
+            if (componentId < 0 || componentId >= amountOfComponents)
+            {
+                throw new ArgumentOutOfRangeException($"componentid {componentId} is out of range. Range is: 0 -  {amountOfComponents - 1 }");
+            }
+
             var result = new List<int>();
-            for (int i = 0; i < componentIds.Length; i++) {
+            for (int i = 0; i < componentIds.Length; i++)
+            {
                 if (componentIds[i] == componentId)
                 {
                     result.Add(i);
@@ -37,7 +76,6 @@ namespace Fluttert.DirectGraph
             }
             return result;
         }
-
 
         private void ProcessGraph(UndirectedGraph graph)
         {
